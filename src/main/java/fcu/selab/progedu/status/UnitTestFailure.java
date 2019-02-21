b2183dcbf4a305901bc312c8a77ce4a08d5b6ce7
Moty <zzser15963@gmail.com>
2019-02-21 23:41:48 +0800

    UnitTestFailure.java 測試完成

--- src/main/java/fcu/selab/progedu/status/UnitTestFailure.java
+++ src/main/java/fcu/selab/progedu/status/UnitTestFailure.java
@@ -3,26 +3,26 @@ package fcu.selab.progedu.status;
 public class UnitTestFailure implements Status {
   @Override
   public String getConsole(String consoleText) {
-    String Tests = "";
+    String tests = "";
     String unitTest = "";
     String startStr = "-------------------------------------------------------\r\n" + " T E S T S";
-    String goal = "Tests run:";
+    String goal = "tests run:";
     int endStr = consoleText.indexOf(goal, consoleText.indexOf(goal) + goal.length());
     int end = consoleText.indexOf("\n", endStr);
-    Tests = consoleText.substring(endStr, end);
+    tests = consoleText.substring(endStr, end);
 
     String errorsStr = "Errors:";
     String failuresStr = "Failures:";
 
-    int errors_goal = Tests.indexOf(errorsStr) + errorsStr.length();
-    int failures_goal = Tests.indexOf(failuresStr) + failuresStr.length();
+    int errorsGoal = tests.indexOf(errorsStr) + errorsStr.length();
+    int failuresGoal = tests.indexOf(failuresStr) + failuresStr.length();
 
-    String errors = Tests.substring(errors_goal, Tests.indexOf(",", errors_goal)).trim();
-    String failures = Tests.substring(failures_goal, Tests.indexOf(",", failures_goal)).trim();
+    String errors = tests.substring(errorsGoal, tests.indexOf(",", errorsGoal)).trim();
+    String failures = tests.substring(failuresGoal, tests.indexOf(",", failuresGoal)).trim();
 
-    if (errors.equals("0") && failures.equals("0"))
+    if (errors.equals("0") && failures.equals("0")) {
       return "";
-
+    }
     unitTest = consoleText.substring(consoleText.indexOf(startStr), end);
     System.out.println(unitTest);
     return unitTest;
