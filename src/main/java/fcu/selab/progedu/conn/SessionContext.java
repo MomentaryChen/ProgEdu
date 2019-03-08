e4466b9989ccfef4da10cb9eb80aac0843186764
Moty <zzser15963@gmail.com>
2019-03-08 10:28:53 +0800

    add Sesstion ContextListener and SessionContext to Manager Session

---
+++ src/main/java/fcu/selab/progedu/conn/SessionContext.java
@@ -0,0 +1,60 @@
+package fcu.selab.progedu.conn;
+
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+public class SessionContext {
+  
+  private static SessionContext instance ;
+  private HashMap<String, HttpSession> sessionMap;
+  
+  private SessionContext() {
+    sessionMap = new HashMap<String, HttpSession>();
+  }
+  
+  /** only one SesstionContext .
+   * 
+   * @return instance 
+   */
+  public static SessionContext getInstance() {
+    if (instance == null) {
+      return new SessionContext();
+    } else {
+      return instance;
+    }
+  }
+  
+  /**add the session into the HashMap .
+   * 
+   * @param session is the new.
+   */
+  public synchronized void addSession(HttpSession session) {
+    if (session != null) {
+      sessionMap.put(session.getId(), session);
+    }
+  }
+  /**use the session id to get the session.
+   * 
+   * @param session is .
+   */
+  
+  public synchronized HttpSession getSession(HttpSession session) {
+    if (session != null) {
+      return sessionMap.get(session.getId());
+    } else {
+      return null;
+    }
+  }
+  /**delete the session from sessionMap.
+   * 
+   * @param session is been deleted.
+   */
+  
+  public synchronized void delSession(HttpSession session) {
+    if (session != null) {
+      sessionMap.remove(session.getId());
+    }
+  }
+
+}
---
+++ src/main/java/fcu/selab/progedu/conn/SessionListener.java
@@ -0,0 +1,28 @@
+package fcu.selab.progedu.conn;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class SessionListener implements HttpSessionListener {
+
+  private SessionContext sessionContext = SessionContext.getInstance(); 
+
+  /**created the session
+   * 
+   */
+  
+  public void sessionCreated(HttpSessionEvent se) {
+    HttpSession session = se.getSession();
+    sessionContext.addSession(session);
+  }
+  /** destroyed the session
+   * 
+   */ 
+  
+  public void sessionDestroyed(HttpSessionEvent se) {
+    HttpSession session = se.getSession();
+    sessionContext.delSession(session);
+  }
+  
+}
